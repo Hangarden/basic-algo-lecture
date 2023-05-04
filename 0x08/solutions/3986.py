242c3819510fa253b1fdea4533a9ecaef9a6ee90
YongHoon <akatcn@gmail.com>
2023-05-04 20:46:03 +0900

    0x08 스택의 활용 문제 풀이

--- 0x08/solutions/3986.py
+++ 0x08/solutions/3986.py
@@ -1,1 +1,18 @@
+#--- 입력 파트 ---#
+N = int(input())
 
+#--- 로직 파트 ---#
+ans = 0
+for _ in range(N):
+    s = input()
+    stack = []
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    if not stack:
+        ans += 1
+
+#--- 출력 파트 ---#
+print(ans)
--- 0x08/solutions/4949.py
+++ 0x08/solutions/4949.py
@@ -1,1 +1,32 @@
+import sys
 
+while True:
+    #--- 입력 파트 ---#
+    s = sys.stdin.readline().rstrip()
+    if s == '.': break
+
+    #--- 로직 파트 ---#
+    stack = []
+    ans = "yes"
+    for c in s:
+        if c == '(' or c == '[':
+            stack.append(c)
+        elif c == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+                continue
+            else:
+                ans = "no"
+                break
+        elif c == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+                continue
+            else:
+                ans = "no"
+                break
+    #--- 출력 파트 ---#
+    if stack:
+        print("no")
+    else:
+        print(ans)
