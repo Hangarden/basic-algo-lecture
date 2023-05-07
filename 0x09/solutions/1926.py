cafc1f959d152c949221121abcba52c232f3a206
YongHoon <akatcn@gmail.com>
2023-05-07 23:03:14 +0900

    0x09 BFS 문제 2개 풀이

--- 0x09/solutions/1926.py
+++ 0x09/solutions/1926.py
@@ -1,1 +1,38 @@
+# BFS
+from collections import deque
 
+#--- 입력 파트 ---#
+N, M = map(int, input().split())
+matrix= []
+for _ in range(N):
+    matrix.append(list(map(int, input().split())))
+
+#--- 로직 파트 ---#
+drs = [-1, 0, 1, 0]
+dcs = [0, 1, 0, -1]
+count = 0
+max_area = 0
+dq = deque()
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 1:
+            count += 1
+            dq.append([i, j])
+            matrix[i][j] = 0
+            area = 1
+            while dq:
+                polled = dq.popleft()
+                r = polled[0]
+                c = polled[1]
+                for d in range(4):
+                    dr = r + drs[d]
+                    dc = c + dcs[d]
+                    if 0<=dr<N and 0<=dc<M and matrix[dr][dc] == 1:
+                        area += 1
+                        dq.append([dr, dc])
+                        matrix[dr][dc] = 0
+            max_area = max(max_area, area)
+            
+#--- 출력 파트 ---#
+print(count)
+print(max_area)
--- 0x09/solutions/2178.py
+++ 0x09/solutions/2178.py
@@ -1,1 +1,38 @@
+# DFS
+from collections import deque
 
+#--- 입력 파트 ---#
+N, M = map(int, input().split())
+matrix = [[0 for col in range(M)] for row in range(N)]
+for i in range(N):
+    s = input()
+    for j in range(M):
+        matrix[i][j] = int(s[j])
+
+#--- 로직 파트 ---#
+dq = deque()
+dq.append([0,0])
+matrix[0][0] = 0
+drs = [-1, 0, 1, 0]
+dcs = [0, 1, 0, -1]
+length = 0
+while dq:
+    length += 1
+    temp_dq = deque()
+    while dq:
+        polled = dq.popleft()
+        r = polled[0]
+        c = polled[1]
+        if r == N-1 and c == M-1:
+            temp_dq = deque()
+            break
+        for d in range(4):
+            dr = r + drs[d]
+            dc = c + dcs[d]
+            if 0<=dr<N and 0<=dc<M and matrix[dr][dc] == 1:
+                temp_dq.append([dr, dc])
+                matrix[dr][dc] = 0
+    dq = temp_dq
+
+#--- 출력 파트 ---#
+print(length)
