bd59d90edbcb46b22597710e8bdc9b4c1f937975
YongHoon <akatcn@gmail.com>
2023-05-10 22:47:34 +0900

    BFS 1문제, 재귀 1문제 풀이

--- 0x09/solutions/4179.py
+++ 0x09/solutions/4179.py
@@ -1,1 +1,61 @@
+import sys
+from collections import deque
 
+#--- 입력 파트 ---#
+N, M = map(int, input().split())
+matrix = [['' for col in range(M)] for row in range(N)]
+jdq = deque()
+fdq = deque()
+
+for i in range(N):
+    s = sys.stdin.readline().rstrip()
+    for j in range(M):
+        matrix[i][j] = s[j]
+        if matrix[i][j] == 'J':
+            jdq.append([i, j])
+        elif matrix[i][j] == 'F':
+            fdq.append([i, j])
+
+#---로직 파트 ---#
+drs = [-1, 0, 1, 0]
+dcs = [0, 1, 0, -1]
+time = 0
+ans = -1
+while jdq:
+    time += 1
+    temp_jdq = deque()
+    temp_fdq = deque()
+    while fdq:
+        polled = fdq.popleft()
+        r = polled[0]
+        c = polled[1]
+        for d in range(4):
+            dr = r + drs[d]
+            dc = c + dcs[d]
+            if 0 <= dr < N and 0 <= dc < M and (matrix[dr][dc] == 'J' or matrix[dr][dc] == '.'):
+                matrix[dr][dc] = 'F'
+                temp_fdq.append([dr,dc])
+    fdq = temp_fdq
+    while jdq:
+        polled = jdq.popleft()
+        r = polled[0]
+        c = polled[1]
+        for d in range(4):
+            dr = r + drs[d]
+            dc = c + dcs[d]
+            if 0 <= dr < N and 0 <= dc < M:
+                if matrix[dr][dc] == '.':
+                    matrix[dr][dc] = 'J'
+                    temp_jdq.append([dr, dc])
+            else:
+                ans = time
+                break
+    if ans > 0:
+        break
+    jdq = temp_jdq
+
+#---출력 파트 ---#
+if ans == -1:
+    print('IMPOSSIBLE')
+else:
+    print(ans)
--- 0x0B/solutions/1074.py
+++ 0x0B/solutions/1074.py
@@ -1,1 +1,30 @@
+"""
+  1구역  |  2구역
+-------------------
+  3구역  |  4구역
+"""
 
+#--- 입력 파트 ---#
+N, R, C = map(int, input().split())
+
+#---로직 파트 ---#
+def recursion(size, start_num, start_row, start_col):
+    if size == 1:
+        return start_num
+    if R < start_row + size // 2:
+        if C < start_col + size // 2:  # 1구역
+            num = start_num
+            return recursion(size // 2, num, start_row, start_col)
+        else:  # 2구역
+            num = start_num + (size // 2) * (size // 2)
+            return recursion(size // 2, num, start_row, start_col + size//2)
+    else:
+        if C < start_col + size // 2:  # 3구역
+            num = start_num + ((size // 2) * (size // 2)) * 2
+            return recursion(size // 2, num, start_row + size//2, start_col)
+        else:  # 4구역
+            num = start_num + ((size // 2) * (size // 2)) * 3
+            return recursion(size // 2, num, start_row + size//2, start_col + size//2)
+
+#---출력 파트 ---#
+print(recursion(2**N, 0, 0, 0))
