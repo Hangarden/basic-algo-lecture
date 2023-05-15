f29b71e6c6a667873ff0eeeae9f79bcad2e3b0eb
YongHoon <akatcn@gmail.com>
2023-05-15 21:17:54 +0900

    BFS 1문제, 재귀 1문제 풀이

--- 0x09/solutions/7569.py
+++ 0x09/solutions/7569.py
@@ -1,1 +1,49 @@
+import sys
+from collections import deque
 
+#---입력 파트---#
+M, N, H = map(int, input().split())
+matrix = [[[0 for col in range(M)] for row in range(N)] for height in range(H)]
+dq = deque()
+for l in range(H):
+    for i in range(N):
+        nums = list(map(int, sys.stdin.readline().rstrip().split()))
+        for j in range(M):
+            if nums[j] == 1:
+                dq.append([l, i, j])
+            matrix[l][i][j] = nums[j]
+
+#---로직 파트---#
+count = -1
+dhs = [0, 0, 0, 0, -1, 1]
+drs = [-1, 0, 1, 0, 0, 0]
+dcs = [0, 1, 0, -1, 0, 0]
+while dq:
+    count += 1
+    temp_dq = deque()
+    while dq:
+        polled = dq.popleft()
+        h = polled[0]
+        r = polled[1]
+        c = polled[2]
+        for d in range(6):
+            dh = h + dhs[d]
+            dr = r + drs[d]
+            dc = c + dcs[d]
+            if 0<=dh<H and 0<=dr<N and 0<=dc<M and matrix[dh][dr][dc] == 0:
+                matrix[dh][dr][dc] = 1
+                temp_dq.append([dh, dr, dc])
+    dq = temp_dq
+
+#---출력 파트---#
+for l in range(H):
+    for i in range(N):
+        for j in range(M):
+            if matrix[l][i][j] == 0:
+                count = -1
+                break
+        if count == -1:
+            break
+    if count == -1:
+        break
+print(count)
--- 0x0B/solutions/1992.py
+++ 0x0B/solutions/1992.py
@@ -1,1 +1,28 @@
+import sys
 
+#---입력 파트---#
+N = int(input())
+matrix = [['' for col in range(N)] for row in range(N)]
+for i in range(N):
+    s = sys.stdin.readline().rstrip()
+    for j in range(N):
+        matrix[i][j] = s[j]
+
+#---로직 파트---#
+def recursion(size, r, c):
+    if size == 1:
+        return matrix[r][c]
+    else:
+        left_top = recursion(size//2, r, c)
+        right_top = recursion(size//2, r, c + size//2)
+        left_bottom = recursion(size//2, r + size//2, c)
+        right_bottom = recursion(size//2, r + size//2, c + size//2)
+        if left_top == right_top == left_bottom == right_bottom == '0':
+            return '0'
+        elif left_top == right_top == left_bottom == right_bottom == '1':
+            return '1'
+        else:
+            return '(' + left_top + right_top + left_bottom + right_bottom + ')'
+
+#---출력 파트---#
+print(recursion(N, 0, 0))
