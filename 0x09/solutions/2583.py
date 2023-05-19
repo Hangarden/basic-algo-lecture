3e9977ff821a384549a8d2d72f5c25ce31abfbf0
YongHoon <akatcn@gmail.com>
2023-05-19 21:14:56 +0900

    BFS 1문제, 재귀 1문제 풀이

--- 0x09/solutions/2583.py
+++ 0x09/solutions/2583.py
@@ -1,1 +1,42 @@
+import sys
+from collections import deque
 
+#---입력 파트---#
+N, M, K = map(int, input().split())
+matrix = [[0 for col in range(M)] for row in range(N)]
+for _ in range(K):
+    j1, i1, j2, i2 = map(int, sys.stdin.readline().rstrip().split())
+    for i in range(i1, i2):
+        for j in range(j1, j2):
+            matrix[i][j] = 1
+
+#---로직 파트---#
+drs = [-1, 0, 1, 0]
+dcs = [0, 1, 0, -1]
+count = 0
+ans = []
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 0:
+            count += 1
+            dq = deque()
+            dq.append([i, j])
+            matrix[i][j] = 1
+            area = 1
+            while dq:
+                polled = dq.popleft()
+                r = polled[0]
+                c = polled[1]
+                for d in range(4):
+                    dr = r + drs[d]
+                    dc = c + dcs[d]
+                    if 0<=dr<N and 0<=dc<M and matrix[dr][dc] == 0:
+                        dq.append([dr, dc])
+                        matrix[dr][dc] = 1
+                        area += 1
+            ans.append(area)
+
+#---출력 파트---#
+print(count)
+for num in sorted(ans):
+    print(num, end=" ")
--- 0x0B/solutions/2448.py
+++ 0x0B/solutions/2448.py
@@ -1,1 +1,22 @@
+#--- 입력 파트 ---#
+N = int(input())
 
+#--- 로직 파트 ---#
+matrix = [[' ' for col in range(2*N-1)] for row in range(N)]
+def recursion(size, r, c):
+    if size == 3:
+        matrix[r][c] = "*"
+        matrix[r+1][c-1] = "*"
+        matrix[r+1][c+1] = "*"
+        for j in range(c-2, c+3):
+            matrix[r+2][j] = "*"
+    else:
+        recursion(size//2, r, c)
+        recursion(size//2, r + size//2, c-size//2)
+        recursion(size//2, r + size//2, c+size//2)
+recursion(N, 0, N-1)
+
+#--- 출력 파트 ---#
+for i in range(N):
+    stars_line = ''.join(matrix[i])
+    print(stars_line)
