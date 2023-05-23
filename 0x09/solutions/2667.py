86607526d913d29899e8029c83975277e60b9901
YongHoon <akatcn@gmail.com>
2023-05-23 20:22:10 +0900

    BFS 2문제 풀이

--- 0x09/solutions/2667.py
+++ 0x09/solutions/2667.py
@@ -1,1 +1,38 @@
+from collections import deque
 
+#--- 입력 파트 ---#
+N = int(input())
+matrix = [[0 for col in range(N)] for row in range(N)]
+for i in range(N):
+    s = input()
+    for j in range(N):
+        matrix[i][j] = int(s[j])
+
+#--- 로직 파트 ---#
+drs = [-1, 0, 1, 0]
+dcs = [0, 1, 0, -1]
+counts = []
+for i in range(N):
+    for j in range(N):
+        if matrix[i][j] == 1:
+            count = 1
+            dq = deque()
+            dq.append([i, j])
+            matrix[i][j] = 0
+            while dq:
+                polled = dq.popleft()
+                r = polled[0]
+                c = polled[1]
+                for d in range(4):
+                    dr = r + drs[d]
+                    dc = c + dcs[d]
+                    if 0<=dr<N and 0<=dc<N and matrix[dr][dc] == 1:
+                        count += 1
+                        dq.append([dr, dc])
+                        matrix[dr][dc] = 0
+            counts.append(count)
+
+#--- 출력 파트 ---#
+print(len(counts))
+for count in sorted(counts):
+    print(count)
--- 0x09/solutions/5014.py
+++ 0x09/solutions/5014.py
@@ -1,1 +1,36 @@
+# BFS
+from collections import deque
 
+#--- 입력 파트 ---#
+F, S, G, U, D = map(int, input().split())
+
+#--- 로직 파트 ---#
+dq = deque()
+dq.append(S)
+count = 0
+ans = -99
+visited = dict()
+visited[S] = True
+while dq:
+    temp_dq = deque()
+    count += 1
+    while dq:
+        polled = dq.popleft()
+        if polled == G:
+            temp_dq = deque()
+            ans = count-1
+            break
+        else:
+            if polled+U <= F and polled+U not in visited:
+                temp_dq.append(polled+U)
+                visited[polled+U] = True
+            if polled-D >= 1 and polled-D not in visited:
+                temp_dq.append(polled-D)
+                visited[polled-D] = True
+    dq = temp_dq
+
+#--- 출력 파트 ---#
+if ans == -99:
+    print("use the stairs")
+else:
+    print(ans)
